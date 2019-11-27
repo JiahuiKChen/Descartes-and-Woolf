38f6c7b306e32e60f566a982da3b670367a651cb
JiahuiKChen <jiahui.k.chen@gmail.com>
2019-11-27 00:44:33 -0700

    Web app architecture started!

---
+++ app/main.py
@@ -0,0 +1,53 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+# Loading all sentences and word sets for authors
+text_dicts_dir = "AuthorTextDicts/"
+with open(text_dicts_dir + "Woolf-Sentences.pickle", 'rb') as woolf_file:
+    woolf_sentences = pickle.load(woolf_file)
+    print(len(woolf_sentences))
+    
+with open(text_dicts_dir + "Woolf-WordSets.pickle", 'rb') as woolf_file:
+    woolf_wordsets = pickle.load(woolf_file)
+    print(len(woolf_wordsets))
+    
+with open(text_dicts_dir + "Descartes-Sentences.pickle", 'rb') as d_file:
+    descartes_sentences = pickle.load(d_file)
+    print(len(descartes_sentences))
+    
+with open(text_dicts_dir + "Descartes-WordSets.pickle", 'rb') as d_file:
+    descartes_wordsets = pickle.load(d_file)
+    print(len(descartes_wordsets))
+
+    
+def get_author_sentences(phrase, wordsets, sentences):   
+    found_ids = []
+    found_sentences = []
+    for s_id in wordsets:
+        if phrase in wordsets[s_id]:
+            found_ids.append(s_id)
+    
+    for s_id in found_ids:
+        found_sentences.append(sentences[s_id])
+    result_str = "\n\n".join(found_sentences)
+    return result_str
+
+# Searches for an input phrase, returns sentences found from each author (as one big string each)
+def text_search(phrase):
+    txt_input = phrase.strip()
+    woolf_results = ""
+    descartes_results = ""
+    if len(text_input) == 0:
+        woolf_results = "Input was empty! Try again"
+        descartes_results = "Input was empty! Try again"
+    else:
+        woolf_result = get_author_sentences(txt_input, woolf_wordsets, woolf_sentences)
+        descartes_result = get_author_sentences(txt_input, descartes_wordsets, descartes_sentences)
+        
+        
+@app.route('/')
+def main_page():
+    render_template('home.html')
