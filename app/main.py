5119863e6bc1e3aa69135e8b82642314f691a1b4
JiahuiKChen <jiahui.k.chen@gmail.com>
2019-11-28 16:34:53 -0700

    Final touches on display for text bots page

--- app/main.py
+++ app/main.py
@@ -70,7 +70,6 @@ def textbots_page():
     return render_template('text-bots.html')
 
 
-
 # UNCOMMENT FOR LOCAL RUN ONLY
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+# if __name__ == '__main__':
+#     app.run(host='127.0.0.1', port=8080, debug=True)
