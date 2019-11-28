21a5f8ddadb80feb11c920088fadc236d15c214c
JiahuiKChen <jiahui.k.chen@gmail.com>
2019-11-27 23:28:25 -0700

    TEXT SEARCH FULLY WORKS! Scrolling and correctly spaced results displayed

--- app/main.py
+++ app/main.py
@@ -28,38 +28,40 @@ def get_author_sentences(phrase, wordsets, sentences):
 
     for s_id in found_ids:
         found_sentences.append(sentences[s_id])
-    result_str = "\n\n".join(found_sentences)
+    # result_str = "\n\n".join(found_sentences)
     # result_str = "<br><br>".join(found_sentences)
-    return result_str
+    # return result_str
+    return found_sentences
 
 # Searches for an input phrase, returns sentences found from each author (as one big string each)
 def text_search(phrase):
     txt_input = phrase.strip()
-    woolf_results = ""
-    descartes_results = ""
+    woolf_result = ""
+    descartes_result = ""
     if len(txt_input) == 0:
-        woolf_results = "Input was empty! Try again"
-        descartes_results = "Input was empty! Try again"
+        woolf_result = "Input was empty! Try again"
+        descartes_result = "Input was empty! Try again"
     else:
         woolf_result = get_author_sentences(txt_input, woolf_wordsets, woolf_sentences)
         descartes_result = get_author_sentences(txt_input, descartes_wordsets, descartes_sentences)
-    if len(woolf_result) == 0:
-        woolf_result = "None of Woolf's writing contains this phrase..."
-    if len(descartes_result) == 0:
-        descartes_result = "None of Descartes' writing contains this phrase..."
-    return descartes_result, woolf_result      
+        if len(woolf_result) == 0:
+            woolf_result = ["None of Woolf's writing contains this phrase..."]
+        if len(descartes_result) == 0:
+            descartes_result = ["None of Descartes' writing contains this phrase..."]
+    return descartes_result, woolf_result
 
 
 @app.route('/')
 def main_page():
-#     return str(len(descartes_wordsets))
     return render_template('text-search.html')
 
 # Method that gets input from user and returns strings found from text
 @app.route('/', methods=['POST'])
-def get_results():
+def get_results():  
     search_input = request.form['text-input']
     descartes_result, woolf_result = text_search(search_input)
+    if not (isinstance(descartes_result, list) and isinstance(woolf_result, list)):
+        return render_template('text-search-error.html', issue=descartes_result)
     return render_template('text-results.html', descartes_result=descartes_result,
         woolf_result=woolf_result)
 
